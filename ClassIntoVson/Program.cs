6792b97686517d345f23ebaf7335e108e366a4f8
Erkan Ergen <erkanergen35@gmail.com>
2021-02-13 10:46:54 +0300

    Add project files.

---
+++ ClassIntoVson/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ClassIntoVson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Erkan ERGEN";
+            kurs1.IzlenmeOranı = 86;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "JAVA  ";
+            kurs2.Egitmen = "Ada ERGEN";
+            kurs2.IzlenmeOranı = 96;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Pyton";
+            kurs3.Egitmen = "Aras ERGEN";
+            kurs3.IzlenmeOranı = 106;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.KursAdi = "C++";
+            kurs4.Egitmen = "Kevin";
+            kurs4.IzlenmeOranı = 100;
+
+            //nasıl string de int te array yapıyorsak class ta da array olarak tutabiliriz bu kıslmı. 
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3, kurs4 };
+
+
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + "      :     " + kurs.Egitmen + "   ,   " + kurs.IzlenmeOranı + "  ,   " + kurs.BegeniOranı);  // beğeni oranı class içlerine eklenmediğinden + dan sonra yazmıiyor. 
+            }
+        }
+    }
+    //string gibi int gibi aslında bir değişken tanımladık ve adına da Kurs koyduk .
+    class Kurs
+
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOranı { get; set; }
+        public string BegeniOranı { get; set; }
+    }
+}
+
---
+++ ForDonguleriVson/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ForDonguleriVson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //type safety tip güvenliği demektir. 
+            //Donot repeat yourself. 
+            //değişken tanımladık aşağıda. 
+
+            string kategoriEtiketi = "Kategoriler";
+            int ögrenciSayısı = 32000;
+            double faizOranı = 1.45;
+            bool sistemeGirisYapmisMi = true; // burada 0,1 doğru yanlış değişkeni demektir bu. 
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Sisteme Giriş Yapildi");
+            }
+            else
+            {
+                Console.WriteLine("Sisteme Giriş Yapiniz");
+            }
+
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+            Console.WriteLine(kategoriEtiketi);
+
+
+        }
+    }
+}
