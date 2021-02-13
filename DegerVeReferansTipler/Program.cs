164c81876d9bb95950394202bcc723caa596d795
Erkan Ergen <erkanergen35@gmail.com>
2021-02-13 17:16:41 +0300

    Referans tipler

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -8,7 +8,21 @@ namespace DegerVeReferansTipler
         {
             Console.WriteLine("Hello World!");
 
-            int sayı1
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+                //sayı 1 = ?   30
+
+                //new dediğimizde bellekte yeni bir alan aç demiş oluyoruz. 
+                 int[] sayılar1 = new int[] { 10, 20, 30 };
+                 int[] sayılar2 = new int[] { 100, 200, 300 };
+            sayılar1 = sayılar2;
+                sayılar2[0] = 999;
+            //sayilar1[0]=?    999 
+
+
+
         }
     }
 }
